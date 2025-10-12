336db503b4188bba260f71d03d3586c6184f9207
Krish Shah <krishah247@gmail.com>
2025-10-11 23:37:30 -0400

    Started UI on welcome page

---
+++ app/src/main/java/com/example/otams/WelcomeActivity.java
@@ -0,0 +1,12 @@
+package com.example.otams;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WelcomeActivity extends AppCompatActivity{
+
+
+}
