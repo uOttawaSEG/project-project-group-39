73a84ecc87f1d5f79939d0cce3e6a2ebe02d0505
Nicolas Benavides <76823475+nicob1800@users.noreply.github.com>
2025-10-12 20:25:58 -0400

    almost have the welcome page working. register works but login makes it crash. TRUST THE PROCESS. Inshallah it will be done soon

--- app/src/main/java/com/example/otams/RegisterActivity.java
+++ app/src/main/java/com/example/otams/RegisterActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +17,19 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         ImageButton registerButton = findViewById(R.id.backButt);
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        EditText program = findViewById(R.id.program);
+        EditText highestLevelOfStudy = findViewById(R.id.highestLevelOfStudy);
+        EditText coursesToTeach = findViewById(R.id.coursesToTeach);
+        if(radioGroup.getCheckedRadioButtonId() == R.id.Student) {
+            program.setVisibility(View.VISIBLE);
+            highestLevelOfStudy.setVisibility(View.GONE);
+            coursesToTeach.setVisibility(View.GONE);
+        }else if(radioGroup.getCheckedRadioButtonId() == R.id.Tutor){
+            program.setVisibility(View.GONE);
+            highestLevelOfStudy.setVisibility(View.VISIBLE);
+            coursesToTeach.setVisibility(View.VISIBLE);
+        }
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
